3b7dcf6e81de2cd4014b29a1e17c6df22989e2d2
garasubo <garasubo@gmail.com>
2015-05-18 21:28:38 +0900

    WIP

--- platform/twrls1021a/platform.sh
+++ platform/twrls1021a/platform.sh
@@ -6,7 +6,7 @@ export AR=${AR-arm-none-eabi-ar}
 export OBJCOPY=${OBJCOPY-arm-none-eabi-objcopy}
 
 # arm-none-eabi-gcc does not turn this off when cpu set
-EXTRA_CPPFLAGS="-D__VFP_FP__"
+#EXTRA_CPPFLAGS="-D__VFP_FP__"
 
 EXTRA_CFLAGS="-mcpu=cortex-a7"
 EXTRA_AFLAGS="-mcpu=cortex-a7"
@@ -19,5 +19,4 @@ EXTRA_LDSCRIPT="-T ${LINKSCRIPT}"
 EXTRA_LDSCRIPT_CC="-Wl,-T,${LINKSCRIPT}"
 
 
-
-EXTRAFLAGS="-F CPPFLAGS=${EXTRA_CPPFLAGS} -F ACFLAGS=${EXTRA_AFLAGS} -F CWARNFLAGS=${EXTRA_CWARNFLAGS} -F CFLAGS=${EXTRA_CFLAGS} -F CPPFLAGS=-mfpu=neon -F ACLFLAGS=-mfpu=neon ${EXTRAFLAGS}"
+EXTRAFLAGS="-F CPPFLAGS=${EXTRA_CPPFLAGS} -F ACFLAGS=${EXTRA_AFLAGS} -F CWARNFLAGS=${EXTRA_CWARNFLAGS} -F CFLAGS=${EXTRA_CFLAGS} -F CPPFLAGS=-mfpu=neon -F ACLFLAGS=-mfpu=neon -F ACLFLAGS=-mfloat-abi=hard -F CPPFLAGS=-mfloat-abi=hard ${EXTRAFLAGS}"
