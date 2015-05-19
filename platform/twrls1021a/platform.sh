10a41c6aedb799ec2ccf4475ef00f45145085e5b
garasubo <garasubo@gmail.com>
2015-05-19 15:08:21 +0900

    use hardware float

--- platform/twrls1021a/platform.sh
+++ platform/twrls1021a/platform.sh
@@ -8,8 +8,8 @@ export OBJCOPY=${OBJCOPY-arm-none-eabi-objcopy}
 # arm-none-eabi-gcc does not turn this off when cpu set
 #EXTRA_CPPFLAGS="-D__VFP_FP__"
 
-EXTRA_CFLAGS="-mcpu=cortex-a7"
-EXTRA_AFLAGS="-mcpu=cortex-a7"
+EXTRA_CFLAGS="-mcpu=cortex-a7 -mfpu=neon -mfloat-abi=hard -marm -mthumb-interwork"
+EXTRA_AFLAGS="-mcpu=cortex-a7 -mfpu=neon -mfloat-abi=hard"
 
 # this compiler is very fussy, planning to fix these issues at some point
 EXTRA_CWARNFLAGS="-Wno-error"
@@ -17,6 +17,3 @@ EXTRA_CWARNFLAGS="-Wno-error"
 LINKSCRIPT="${PWD}/platform/twrls1021a/link.ld"
 EXTRA_LDSCRIPT="-T ${LINKSCRIPT}"
 EXTRA_LDSCRIPT_CC="-Wl,-T,${LINKSCRIPT}"
-
-
-EXTRAFLAGS="-F CPPFLAGS=${EXTRA_CPPFLAGS} -F ACFLAGS=${EXTRA_AFLAGS} -F CWARNFLAGS=${EXTRA_CWARNFLAGS} -F CFLAGS=${EXTRA_CFLAGS} -F CPPFLAGS=-mfpu=neon -F ACLFLAGS=-mfpu=neon -F ACLFLAGS=-mfloat-abi=hard -F CPPFLAGS=-mfloat-abi=hard ${EXTRAFLAGS}"
--- platform/twrls1021a/startup.s
+++ platform/twrls1021a/startup.s
@@ -10,5 +10,15 @@ _Reset:
 	cmp r0, r1
 	blo bss_loop
 
+	mrc p15, 0, r0, c1, c0, 2
+	orr r0, r0, #(1 << 20)
+	orr r0, r0, #(1 << 21)
+	orr r0, r0, #(1 << 22)
+	orr r0, r0, #(1 << 23)
+	mcr p15, 0, r0, c1, c0, 2
+	vmrs r0, fpexc
+	orr r0, r0, #(1 << 30)
+	vmsr fpexc, r0
+
 	bl __libc_start_main
 	b .
